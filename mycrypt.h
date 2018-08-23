5eeb63a9a13e50c337743729095b7f08ac9dfe3b
filipefjn <filipe.fjn@gmail.com>
2018-08-23 20:00:37 -0300

    .

--- mycrypt.h
+++ mycrypt.h
@@ -1,4 +1,14 @@
 #ifndef MYCRYPT_H
 #define MYCRYPT_H
 
+// realizar/desfazer permutacao inicial
+
+// realizar/desfazer permutacao final
+
+// dividir string em blocos
+
+// aplicar uma 'rodada'
+
+//
+
 #endif // MYCRYPT_H
